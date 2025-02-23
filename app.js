81a2fb4dfb85313ac519177902568569db2e9ad9
BrenaSt <brena.st@hotmail.com>
2025-02-23 20:12:27 -0300

    Sincronizando com github 2

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ let amigos = [];
 function adicionarAmigo() {
     let nomeAmigo = document.querySelector('input').value;
     //Validar a entrada:
-    if (nomeAmigo == "" || nomeAmigo == " "){
+    if (nomeAmigo == "" || nomeAmigo == " ") {
         alert('Espaço vazio! Por favor, insira um nome.');
         limparCampo();
     }
     //Atualizar o array de amigos:
-    if (amigos.includes(nomeAmigo)){
+    if (amigos.includes(nomeAmigo)) {
         alert('Esse nome já foi incluído! Digite o nome de outro amigo se desejar.');
         limparCampo();
-    }else {
+    } else {
         amigos.push(nomeAmigo);
         exibirLista(amigos);
     }
@@ -22,9 +22,9 @@ function adicionarAmigo() {
 }
 
 //Função para limpar campo para adição de novos nomes.
-function limparCampo() {
-    let campo = document.querySelector('input');
+let campo = document.querySelector('input');
     campo.value = "";
+    function limparCampo() {
 }
 
 //Função para exibir lista de amigos.
@@ -37,22 +37,22 @@ function exibirLista(listaNomes) {
         let item = document.createElement('li'); // Criando um <li>
         item.textContent = nome; // Definindo o texto do <li>
         listaAmigosAdicionados.appendChild(item); // Adicionando <li> à <ul>
-    } 
+    }
 }
 
 //Função pars sortear um amigo aleatorio da lista.
-function sortearAmigo(){
+function sortearAmigo() {
     //Validar que há amigos disponíveis
-    if (amigos.length==0){
+    if (amigos.length == 0) {
         alert('Você precisa inserir um nome antes de sortear!');
-    }else {
+    } else {
         //Gerar um índice aleatório, que vai de 0 ate a quantidade de elementos presentes na lista de amigos  
         let quntDeElemLista = amigos.length;
-        let indiceAleatorio = Math.floor(Math.random()*quntDeElemLista);
+        let indiceAleatorio = Math.floor(Math.random() * quntDeElemLista);
         let nomeSorteado = amigos[indiceAleatorio];
         let campoSorteio = document.querySelector('h2');
         campoSorteio.innerHTML = 'Seu amigo secreto sorteado é: ' + nomeSorteado;
-    }    
+    }
 }
 
 
